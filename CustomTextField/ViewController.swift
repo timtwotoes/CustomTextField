e3409320b0cac3f0ae2c478f67f14617b74a7755
Tim Wolff <timtwotoes@me.com>
2023-02-01 19:49:14 +0100

    Initial Commit

---
+++ CustomTextField/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CustomTextField
+//
+//  Created by Tim on 01/02/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
